420ac95f276ca5e5cca6ef18d47a1d9253f1fb84
Eduardo Henrique Guimarães Marques <eduardo.guimaraes@academico.ifg.edu.br>
2026-06-12 20:27:16 -0300

    text

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,30 @@
 import React from  "react";
-import {Text, View } from 'react-native';
+import {Text, View ,Image, TextInput} from 'react-native';
 import { style } from "./styles";
+import Logo from  '../../assets/logo.png'
 
 export default function Login (){
     return  (
         <View style={style.container}>
-            <View style={style.boxTop}><Text>Vermelho</Text></View>
-            <View style={style.boxMid}><Text>Azul</Text></View>
-            <View style={style.boxBottom}><Text>Verde</Text></View>
+            <View style={style.boxTop}>
+                <Image 
+                    source={Logo} 
+                    style={style.logo}
+                    resizeMode="contain"
+                />
+                <Text style={style.text}>Bem vindo de volta!</Text>
+                </View>
+            <View style={style.boxMid}>
+                <Text style={style.titleInput}>
+                    ENDEREÇO DE E-MAIL                
+                </Text>
+                <TextInput style={style.boxInput} />
+                <Text style={style.titleInput}>
+                    SENHA
+                </Text>
+                <TextInput style={style.boxInput}/> 
+            </View>
+            <View style={style.boxBottom}></View>
         </View>
     )
 
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,32 +1,51 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+import { themas } from "../../global/themes";
 
 export const style = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'black',
-        padding: 20
+        backgroundColor: 'White',
     },
     boxTop:{
         width: '100%',
-        height: 200,
-        backgroundColor: 'red',
+        height: Dimensions.get('window').height/3,
+        alignItems: 'center',
+        justifyContent: 'center'
 
 
     },
     boxMid: {
         width: '100%',
-
-        height: 200,
-        backgroundColor: 'white',
+        height: Dimensions.get('window').height/4,
+        paddingHorizontal: 37,
 
     },
     boxBottom: {
         width: '100%',
+        height: Dimensions.get('window').height/3,
 
-        height: 200,
-        backgroundColor: 'blue',
-
+    }, logo:{
+        width: 80,
+        height: 80,
+    },
+    text : {
+        fontWeight:'bold',
+        marginTop: 40,
+        fontSize: 18
+    },
+    titleInput: {
+        marginLeft: 5,
+        color: themas.colors.gray,
+        marginTop: 20
+    },
+    boxInput: {
+        width: '100%',
+        height: 40,
+        borderWidth: 1,
+        marginTop: 10,
+        borderRadius: 8,
+        flexDirection: 'row'
     }
 });
